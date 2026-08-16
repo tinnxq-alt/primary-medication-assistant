11df8a9f59d9999a920bf44fb5f4a49bb0f40a3c
tinnxq-alt <tinnxq@gmail.com>
2026-08-16 13:30:57 +0800

    支持可编辑的未核验自动填充：更新 service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "primary-medication-pro-v13";
+const CACHE_NAME = "primary-medication-pro-v14";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./drugs.js", "./app.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
